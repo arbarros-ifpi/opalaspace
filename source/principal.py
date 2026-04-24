7ff6476aa1824bffafd8dab38d938bee58382f02
Anderson <anderson.barros@ifpi.edu.br>
2026-04-24 10:00:39 -0300

    Primeira versão do jogo

---
+++ source/principal.py
@@ -0,0 +1,17 @@
+import pygame
+try:
+    # Inicializa o jogo
+    pygame.init()
+except:
+    # Encerra o jogo
+    pygame.quit()
+
+#largura e altura da janela
+largura = 1280
+altura = 720
+
+#configura a janela e o texto do jogo
+screen = pygame.display.set_mode((largura,altura))
+pygame.display.set_caption('Jogo Opala Space')
+
+pygame.display.update()
